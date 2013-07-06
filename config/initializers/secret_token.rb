6b215794728dc3d295a50a507cbaf5ada29dc278
nikita <grodnitskiy@gmail.com>
2013-07-06 16:50:51 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotelAdvisor::Application.config.secret_token = 'a1207947950e863d9a486890dc114dadbce3266bbcbca2539c0829286c78a6d7e47af01aadd853bfe7855f29effda88bdf90507cb05b1122532e9e0a0a0e7b8b'
