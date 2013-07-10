2b48c0d5ec2c901ad934eca85e4bf3ca1637ce19
nikita <grodnitskiy@gmail.com>
2013-07-10 20:01:36 +0300

    upd model

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,5 @@
 class Hotel < ActiveRecord::Base
   attr_accessible :address_city, :address_country, :address_state, :address_street, :breakfast, :photo_hotel, :price_for_room, :rating, :room_desc, :title
   mount_uploader :photo_hotel, PhotoHotelUploader
+  has_many :messages
 end
